67391ad48b96a9983248c89e1975fc94a07f3ecd
kraemerd17 <kraemerd17@grinnell.edu>
2017-03-07 01:24:36 -0600

    Simple ARIMA model implementation.

---
+++ scripts/simple_model.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_context('talk')
+sns.set_style('white')
+
+unrate = pd.read_csv("../data/UNRATE.csv", index_col=0)
+
+tslice = 48 
+test = 20 
+
+p, q, r = 4, 1, 0
+
+unrate = unrate.iloc[-tslice:]
+unrate.index = unrate.index.to_datetime()
+
+pred_range = unrate.index[-test:]
+
+model = sm.tsa.ARIMA(unrate.diff().iloc[1:-test], (p,q,r))
+result = model.fit()
+
+forecast, stderr, conf_int = result.forecast(test)
+
+simulated = unrate.iloc[-test].values + forecast
+
+fig, ax = plt.subplots(1, 1)
+
+ax.plot(unrate, 'k-', label='Unemployemnt Rate')
+ax.plot(pred_range, simulated, 'b', label='Prediction (with 95% CI)')
+ax.plot(pred_range, simulated + conf_int[:,0], 'b--')
+ax.plot(pred_range, simulated + conf_int[:,1], 'b--')
+
+ax.fill_between(pred_range, simulated, simulated + conf_int[:,0],
+        where=simulated + conf_int[:,0] <= simulated, facecolor='blue',
+        interpolate=True, alpha=0.1)
+
+ax.fill_between(pred_range, simulated, simulated + conf_int[:,1],
+        where=simulated + conf_int[:,1] >= simulated, facecolor='blue',
+        interpolate=True, alpha=0.1)
+
+ax.set_xlabel('Year')
+ax.set_ylabel('Unemployment rate (National)')
+ax.set_title('Unemployment rate with ARIMA({},{},{}) model prediction'.format(p,q,r))
+ax.legend()
+sns.despine()
+
+plt.savefig('../figures/ur-arima({},{},{}).svg'.format(p,q,r), bbox_inches='tight')
+
