596071c63e299210d93513d3675115ba1cc9e62e
kraemerd17 <kraemerd17@grinnell.edu>
2017-03-09 01:27:08 -0600

    Fixed the ARIMA models so that it is properly doing fitting on the *difference* and transforming back into the normal space.

--- scripts/simple_model.py
+++ scripts/simple_model.py
@@ -9,22 +9,26 @@ sns.set_style('white')
 
 unrate = pd.read_csv("../data/UNRATE.csv", index_col=0)
 
-tslice = 48 
-test = 20 
+tslice = 40 
+test = 8 
 
 p, q, r = 4, 1, 0
 
 unrate = unrate.iloc[-tslice:]
 unrate.index = unrate.index.to_datetime()
 
-pred_range = unrate.index[-test:]
+pred_range = unrate.index[-test-1:]
 
 model = sm.tsa.ARIMA(unrate.diff().iloc[1:-test], (p,q,r))
 result = model.fit()
 
-forecast, stderr, conf_int = result.forecast(test)
+conf_int = np.empty((test+1,2))
+conf_int[0] = (0,0)
+forecast, stderr, conf_int[1:,:] = result.forecast(test)
 
-simulated = unrate.iloc[-test].values + forecast
+simulated = np.empty(test+1)
+simulated[0] = unrate.iloc[-test-1].values
+simulated[1:] = unrate.iloc[-test].values + forecast.cumsum()
 
 fig, ax = plt.subplots(1, 1)
 
@@ -41,7 +45,7 @@ ax.fill_between(pred_range, simulated, simulated + conf_int[:,1],
         where=simulated + conf_int[:,1] >= simulated, facecolor='blue',
         interpolate=True, alpha=0.1)
 
-ax.set_xlabel('Year')
+ax.set_xlabel('Month')
 ax.set_ylabel('Unemployment rate (National)')
 ax.set_title('Unemployment rate with ARIMA({},{},{}) model prediction'.format(p,q,r))
 ax.legend()
